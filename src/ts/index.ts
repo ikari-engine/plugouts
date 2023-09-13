b8c63e036ecea47ca92bab4f25521a634891734b
lengors <24527258+lengors@users.noreply.github.com>
2023-09-13 23:58:51 +0100

    feat(testing): setup testing tools

--- src/ts/index.ts
+++ src/ts/index.ts
@@ -1,10 +1,11 @@
-import * as core from "@actions/core";
-import { GlobalConfig, Context } from "semantic-release";
+// import * as core from "@actions/core";
+import { Context } from "semantic-release";
 
-export async function success(
-  _: GlobalConfig,
-  context: Context
-): Promise<void> {}
+export async function success(_?: unknown, context?: Context): Promise<void> {
+  if (context !== undefined) {
+    context.logger.log("output something");
+  }
+}
 
 export default {
   success,
