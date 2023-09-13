d40e6e0260310ceac0a669b6ba1c1d8ff9dec55f
lengors <24527258+lengors@users.noreply.github.com>
2023-09-14 00:28:36 +0100

    feat(lint): setup linting

--- src/ts/index.ts
+++ src/ts/index.ts
@@ -1,10 +1,5 @@
 // import * as core from "@actions/core";
-import { PrepareContext, SuccessContext } from "semantic-release";
-
-export async function prepare(_?: unknown, context?: PrepareContext) {
-  if (context !== undefined)
-    context.logger.log(`Releasing version: ${context.nextRelease.version}`);
-}
+import { FailContext, SuccessContext } from "semantic-release";
 
 export async function success(
   _?: unknown,
@@ -15,7 +10,13 @@ export async function success(
   }
 }
 
+export async function fail(_?: unknown, context?: FailContext): Promise<void> {
+  if (context !== undefined) {
+    context.logger.log("output something");
+  }
+}
+
 export default {
-  prepare,
   success,
+  fail,
 };
--- test/ts/index.spec.ts
+++ test/ts/index.spec.ts
@@ -1,11 +1,11 @@
 import gitout from "../../src/ts";
 
 describe("index", () => {
-  it("#prepare", async () => {
-    await gitout.prepare();
-  });
-
   it("#success", async () => {
     await gitout.success();
+  });
+
+  it("#fail", async () => {
+    await gitout.fail();
   });
 });
