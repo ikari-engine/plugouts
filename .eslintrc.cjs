d35081a5457a47e828a8946250f0ac86fc81f1ee
lengors <24527258+lengors@users.noreply.github.com>
2023-09-16 11:49:10 +0100

    fix(gh-actions): fix test and eslint workflows

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -3,4 +3,7 @@ module.exports = {
   parser: "@typescript-eslint/parser",
   plugins: ["@typescript-eslint"],
   root: true,
+  rules: {
+    "no-unused-vars": [2, { args: "all", argsIgnorePattern: "^_" }],
+  },
 };
