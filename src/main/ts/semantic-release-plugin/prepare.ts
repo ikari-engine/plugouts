0f86a9668943e48b503346c0ab5d3c094a78ce7d
lengors <24527258+lengors@users.noreply.github.com>
2023-10-06 19:30:02 +0100

    fix(style): fix

--- src/main/ts/semantic-release-plugin/prepare.ts
+++ src/main/ts/semantic-release-plugin/prepare.ts
@@ -10,9 +10,12 @@ export async function prepare(config: {
   const prettierConfig = (await prettier.resolveConfig(path)) ?? {
     filepath: path,
   };
+  console.log("Checking if CHANGELOG.md is formatted correctly...");
   if (!(await prettier.check(content, prettierConfig))) {
+    console.log("CHANGELOG.md is not formatted correctly. Formatting...");
     const result = await prettier.format(content, prettierConfig);
     await fs.promises.writeFile(path, result);
+    console.log("CHANGELOG.md has been formatted.");
   }
 }
 
